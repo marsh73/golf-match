c6faa9469c0bb5ae4a98f370e89fb552fc60b2c1
Justin Marshall <jmarshall@digitalocean.com>
2019-10-05 08:48:21 -0700

    testing wave

--- api/routes/matchRoutes.js
+++ api/routes/matchRoutes.js
@@ -5,4 +5,16 @@ module.exports = function(app) {
   // todoList Routes
   app.route('/match')
     .post(match.create_match);
+
+  app.post('/hello', (req, res) => {
+    var data = {form: {
+          token: process.env.SLACK_AUTH_TOKEN,
+          channel: "#general",
+          text: "Hi! :wave: \n I'm your new bot."
+        }};
+    request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+          // Sends welcome message
+          res.json();
+        });
+    });
 };
